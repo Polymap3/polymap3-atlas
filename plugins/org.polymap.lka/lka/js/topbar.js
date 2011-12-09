dfb867abca4b41482434bfa0c0fd590be89a4468
fb71 <falko@polymap.de>
2011-12-09 15:41:04 +0100

    #21: Refactoring Atlas client JavaScript (http://polymap.org/atlas/ticket/21)   - set focus on search field after load   - FIX: float left/right   - button icons via CSS background image   - FIX: index.html: use DOCTYPE to get IE out of quirks mode (much better render result, much more comparable to FF rendering results)   - search_pane, search_field ff. CSS from HTML into atlas.layout.css   - FIX: index.html: use text/javascript for javascript; IE ignores them completely otherwise

--- plugins/org.polymap.lka/lka/js/topbar.js
+++ plugins/org.polymap.lka/lka/js/topbar.js
@@ -27,7 +27,7 @@ var Topbar = Class.extend( {
         return this;
     },
 
-    addLeft: function( /*ToolItem*/ item ) {
+    addLeft: function( item ) {
         this.leftItems.push( item );
         return this;
     },
@@ -38,18 +38,23 @@ var Topbar = Class.extend( {
     },
   
     create: function() {
-        this.elm.addClass( 'ui-widget' );  // ui-widget-header
-        this.elm.addClass( 'ui-widget-content' );
-        this.elm.css( "border", "none" );
-        this.elm.css( "margin", "3px" );
-        this.elm.css( "background", "none" );
+        this.elm.addClass( 'ui-widget' )  // ui-widget-header
+                .addClass( 'ui-widget-content' );
+        this.elm.css( "border", "none" )
+                .css( "margin", "3px" )
+                .css( 'display', 'block' )
+                .css( 'height', '16px' )
+                .css( "background", "none" );
         
+        // left items
         for (var i=0; i<this.leftItems.length; i++) {
-            this.createItem( this.elm, this.leftItems[i] );
+            var itemElm = this.createItem( this.elm, this.leftItems[i] );
+            itemElm.css( 'float', 'left' );
             if (i < this.leftItems.length - 1) {
-                this.elm.append( '<span> | </span>' );
+                this.elm.append( '<span style="float:left;"> | </span>' );
             }
         }
+        // right items
         for (var i=this.rightItems.length-1; i >= 0; i--) {
             var itemElm = this.createItem( this.elm, this.rightItems[i] );
             itemElm.css( 'float', 'right' );
@@ -77,7 +82,6 @@ var Topbar = Class.extend( {
         item.elementCreated( button );
         return $( '#' + item.getId() + '_outer' );
     }
-  
 });
 
 
@@ -94,13 +98,13 @@ var Toolbar = Topbar.extend( {
     },
 
     create: function() {
-        this.elm.addClass( 'ui-widget' );
-        this.elm.addClass( 'ui-widget-content' );
-        this.elm.addClass( 'ui-widget-header' );
-        this.elm.addClass( 'ui-corner-top' );
-        //this.elm.css( "border", "none" );
-        this.elm.css( "padding", "2px" );
-        //this.elm.css( "border-bottom", "1px solid #808080" );
+        this.elm.addClass( 'ui-widget' )
+                .addClass( 'ui-widget-content' )
+                .addClass( 'ui-widget-header' )
+                .addClass( 'ui-corner-top' );
+        this.elm.css( 'height', '32px' )
+                .css( 'padding', '0px 1px 0px 1px' )
+                .css( 'display', 'block' );
         
         // left
         for (var i=0; i<this.leftItems.length; i++) {
@@ -108,9 +112,7 @@ var Toolbar = Topbar.extend( {
             var itemElm = mode == "toggle"
                     ? this.createToggle( this.elm, this.leftItems[i] )
                     : this.createButton( this.elm, this.leftItems[i] );
-//            if (i < this.leftItems.length - 1) {
-//                this.elm.append( '<span> | </span>' );
-//            }
+            itemElm.css( "float", "left" );
             this.leftItems[i].elementCreated( itemElm );
         }
         // right
@@ -120,34 +122,48 @@ var Toolbar = Topbar.extend( {
                     ? this.createToggle( this.elm, item )
                     : this.createButton( this.elm, item );
             itemElm.css( "float", "right" );
+                //.css( 'margin-right', 'auto' );
+                //.css( 'margin-left', 'auto' );
             this.rightItems[i].elementCreated( itemElm );
         }
     },
   
-    createButton: function( parent, /*ToolItem*/ item ) {
+    createButton: function( parent, item ) {
         parent.append( '<button id="' + item.getId() + '" title="' + item.getTooltip() + '">' );
-        var button = $( '#' + item.getId() );
+        
+        var button = $( '#'+item.getId() );
         button.button({ label: item.getLabel() });
         button.click( callback( item.onClick, {scope:item, suppressArgs:false} ) );
+        button.css( 'margin', '2px 1px 2px 1px' );
+        
         if (item.icon != null) {
-            button.css( 'display', 'inline' ).css( 'white-space', 'nowrap' ).css( 'width', '90px' );
-            button.prepend( '<img src="' + item.icon + '" style="float:left; margin:5px;">' );
+            button.css( 'display', 'inline' );
+            $('#'+item.getId()+'>span')
+                    .css( 'padding', '5px 9px 4px 26px' )
+                    .css( 'background', 'url(' + item.icon + ') no-repeat scroll 5px 50%' );
+            //button.prepend( '<img src="' + item.icon + '" style="float:left; margin:5px;">' );
         }
         return button;
     },
   
     createToggle: function( parent, item ) {
         parent.append( '<input type="checkbox" id="' + item.getId() + '" title="' + item.getTooltip() + '">'
-                + '<label id="' + item.getId() + "_label" + '" for="' + item.getId() + '">Toggle</label>');
-        var checkbox = $( '#' + item.getId() );
-        var label = $( '#' + item.getId() + "_label" );
-        checkbox.button({ 
-            label: item.getLabel() 
-        });
+                + '<label " for="' + item.getId() + '"></label>');
+        var checkbox = $('#'+item.getId());
+        checkbox.button({ label: item.getLabel() });
         checkbox.click( callback( item.onClick, {scope:item, suppressArgs:false} ) );
+
+        var label = $( '#'+item.getId()+"~label" );
+        label.css( 'margin', '2px 1px 2px 1px' );
+        label.css( 'height', '26px' );
+        
         if (item.icon != null) {
-            label.css( 'white-space', 'nowrap' ).css( 'width', '90px' );
-            label.prepend( '<img src="' + item.icon + '" style="float:left; margin:5px;">' );
+            // space for the icon (span is the label text)
+            $('#'+item.getId()+"~label>span")
+                    .css( 'background', 'url(' + item.icon + ') no-repeat scroll 5px 50%' )
+                    .css( 'padding-left', '26px' )
+                    .css( 'display', 'block' );
+            //label.prepend( '<img src="' + item.icon + '" style="float:left; margin:5px;">' );
         }
         return label;
 
